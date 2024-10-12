28a11212d4e11827f12da53f1e03db4cc42084f7
jumpshot7 <zmamdouh10@gmail.com>
2024-10-12 18:44:51 -0400

    media uplaod

--- src/app/restaurants/login/page.js
+++ src/app/restaurants/login/page.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { auth } from "../../../../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/navigation";
+import "./RestaurantLogin.css";
 
 export default function RestaurantLogin() {
   const [email, setEmail] = useState("");
@@ -20,23 +21,48 @@ export default function RestaurantLogin() {
   };
 
   return (
-    <div>
-      <h1>Restaurant Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <section>
+      <div className="form-box">
+        <div className="form-value">
+          <form onSubmit={handleLogin}>
+            <h2>Login</h2>
+            <div className="inputbox">
+              <ion-icon name="mail-outline"></ion-icon>
+              <input
+                type="email"
+                required
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <label>Email</label>
+            </div>
+            <div className="inputbox">
+              <ion-icon name="lock-closed-outline"></ion-icon>
+              <input
+                type="password"
+                required
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label>Password</label>
+            </div>
+            <div className="forget">
+              <label>
+                <input type="checkbox" /> Remember Me
+              </label>
+              <a href="#">Forget Password</a>
+            </div>
+            <button type="submit">Log in</button>
+            <div className="register">
+              <p>
+                Don't have an account? <a href="#">Register</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 }
