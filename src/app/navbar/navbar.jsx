71ce7b964bc66dc4e024aa5da3720c294ed8be32
CarlitoTF99 <huertero.jose654@gmail.com>
2024-10-12 17:20:59 -0400

    added styles following figma

--- src/app/navbar/navbar.jsx
+++ src/app/navbar/navbar.jsx
@@ -7,11 +7,13 @@ const Navbar = () => {
         <nav className={styles.navbar}>
             <ul className={styles.navList}>
                 <li className={styles.navItem}><a href="/about" className={styles.navLink}>About</a></li>
-                <li className={styles.navItem}><a href="/customers" className={styles.navLink}>Customers</a></li>
-                <li className={styles.navItem}><a href="/restaurants" className={styles.navLink}>Restaurants</a></li>
+                <li className={styles.navItem}><a href="/spotLight" className={styles.navLink}>Spotlight</a></li>
+                <li className={styles.navItem}><a href="/contact" className={styles.navLink}>Contact Us</a></li>
+                <li className={styles.navItem}><a href="/restaurants" className={styles.navLink}>Login</a></li>
+
             </ul>
         </nav>
     );
 }
 
-export default Navbar; // Make sure you're exporting the component
+export default Navbar; 
